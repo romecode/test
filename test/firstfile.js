c4f6c8ccc10db1318412fc0ae1cb525f9767fb20
focus <focus@focus-desktop>
2016-09-09 11:12:15 -0400

    local merge

--- test/firstfile.js
+++ test/firstfile.js
@@ -4,3 +4,9 @@ function one(){
 function two(){
 	console.log('but this is something I changed on another ws');
 }
+function two(){
+	console.log('entirely new shit');
+}
+function two(){
+	console.log('entirely new new shit');
+}
